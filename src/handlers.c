19abd33e243b2a5aae4466d7ecda78e6b1652cb8
matszx <coding@mcygan.anonaddy.me>
2025-02-12 15:56:32 +0100

    fix: a letter

--- src/handlers.c
+++ src/handlers.c
@@ -6,7 +6,7 @@
 /*   By: mcygan <mcygan@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 13:43:44 by mcygan            #+#    #+#             */
-/*   Updated: 2025/02/12 15:29:32 by mcygan           ###   ########.fr       */
+/*   Updated: 2025/02/12 15:55:57 by mcygan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,13 @@ static int	close_handler(t_data *data)
 static int	key_handler(int keycode, t_data *data)
 {
 	if (keycode == 0x77)
-		move_W(data);
+		move_w(data);
 	if (keycode == 0x61)
-		move_A(data);
+		move_a(data);
 	if (keycode == 0x73)
-		move_S(data);
+		move_s(data);
 	if (keycode == 0x64)
-		move_D(data);
+		move_d(data);
 	if (keycode == 0xff53)
 		data->player_a += MOVE_SPEED / 2;
 	if (keycode == 0xff51)
