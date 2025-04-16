9a021c576fef25ca581ba5530203400cae6e144b
matszx <coding@mcygan.anonaddy.me>
2025-04-16 16:28:47 +0200

    checks file extension

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mcygan <mcygan@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 15:30:03 by mcygan            #+#    #+#             */
-/*   Updated: 2025/04/16 14:18:21 by mcygan           ###   ########.fr       */
+/*   Updated: 2025/04/16 16:17:57 by mcygan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,19 @@
 int	main(int argc, char **argv)
 {
 	t_data	data;
+	size_t	pathlen;
 
 	if (argc != 2)
 		close_handler(NULL, "needs single argument");
 	data.fd = open(argv[1], O_RDONLY);
 	if (data.fd < 0)
 		close_handler(NULL, "invalid path");
+	pathlen = ft_strlen(argv[1]);
+	if (pathlen < 5 || ft_strcmp(argv[1] + pathlen - 4, ".cub"))
+	{
+		close(data.fd);
+		close_handler(NULL, "wrong filetype");
+	}
 	init_data(&data);
 	init_events(&data);
 	parse(&data);
